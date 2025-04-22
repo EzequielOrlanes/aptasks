a7fa632ed70c689b480ca0d18fed48b39fec8e44
EzequielOrlanes <you@example.com>
2025-04-22 20:33:15 -0300

    npm

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,12 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:3000' // URL de desenvolvimento
-  : 'https://app103.netlify.app'; // URL de prod
+// const API_URL = process.env.NODE_ENV === 'development' 
+//   ? 'http://localhost:3000' // URL de desenvolvimento
+//   : 'aptasks-production.up.railway.app'; // URL de prod
 
+
+const API_URL = 'aptasks-production.up.railway.app'; // URL de desenvolvimento
 export const registerUser = async (email, password) => {
   try {
     const response = await axios.post(`${API_URL}/register`, {
