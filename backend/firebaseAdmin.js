9b1b33f21bc73f169a319497ef594f2b99a1df75
EzequielOrlanes <you@example.com>
2025-04-25 10:21:42 -0300

    admin-set

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,7 +1,7 @@
 // backend/firebaseAdmin.js
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./seu-arquivo-service-account.json'); // atualize com o caminho certo
+const serviceAccount = require('./hometaskapp-firebase-adminsdk-fbsvc-8216b2542f.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
